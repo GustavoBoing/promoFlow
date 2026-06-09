6051db8475ef36f80655e923caf42e6742c7d1cf
Gustavo <Gustavo.boiing@gmail.com>
2026-06-09 11:09:33 -0300

    feat: Criar as regras de negócio para a classe publicationService, falta apenas inserir exceptions customizadas

--- src/services/PublicationService.py
+++ src/services/PublicationService.py
@@ -1,6 +1,27 @@
+from src.schemas.ProductData import ProductData
+from src.schemas.PromotionData import PromotionData
+
+
 class PublicationService:
     #definir regras de publicação
     #definir o que será publicado
     #evitar duplicidade de publicações
     #loop pelo que não foi publicado
-    pass
+    def evaluate_publication(self, promotion: PromotionData, product: ProductData):
+        if not self.is_already_published(product):
+            raise Exception("Produto já publicado")
+
+        if not self.is_score_valid(promotion):
+            raise Exception("Score é inválido")
+
+        product.publish = True
+
+    def is_already_published(self, product: ProductData):
+        if not product.publish:
+            return True
+        return False
+
+    def is_score_valid(self, promotion: PromotionData):
+        if promotion.score >= 80:
+            return True
+        return False
