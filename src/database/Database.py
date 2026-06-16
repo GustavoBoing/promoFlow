d55e669a34e5f6e224d30b09dbd7b1d17b6c29de
Gustavo <Gustavo.boiing@gmail.com>
2026-06-15 21:29:40 -0300

    feat: Criação das tabelas do banco de dados arquivo: create_db.py

--- src/database/Database.py
+++ src/database/Database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine # gerencia as conexões físicas do banco
 from sqlalchemy.orm import sessionmaker # faz todas as operações do banco de dados(inserir, atualizar, deletar  e buscar)
 from sqlalchemy.orm import declarative_base # Todas as classes que representam as tabelas devem herdar dessa classe
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/PromoFlow"
+DATABASE_URL = "postgresql+pg8000://postgres:admin@localhost:5432/PromoFlow"
 
 engine = create_engine(DATABASE_URL)
 
