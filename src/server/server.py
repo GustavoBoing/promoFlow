39b241bc385599cb60e50b4a1926a6ee5e8458bd
Gustavo <Gustavo.boiing@gmail.com>
2026-06-21 12:24:44 -0300

    ´feat: A api do do mercado livre não consegue aceitar um app que não seja de um vendedor para pegar produtos e exibir as informações do produto, o erro 403 é evidente a cada tentativa de GET

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from src.routes.OffersRoutes import offers_routes
 from src.routes.app import app_route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Promoflow Backend")
 
 app.include_router(offers_routes)
 app.include_router(app_route)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
